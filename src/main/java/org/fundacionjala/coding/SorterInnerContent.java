e21f2cef7f6aef19d13562d137e1a6f769a1c2d4
jccolina <jose.colina@jalasoft.com>
2019-10-02 09:20:38 -0400

    Fixed format and magic number

--- src/main/java/org/fundacionjala/coding/SorterInnerContent.java
+++ src/main/java/org/fundacionjala/coding/SorterInnerContent.java
@@ -1,16 +1,17 @@
 package org.fundacionjala.coding;
 
 public class SorterInnerContent {
+    public static final int MINIMUM_LENGHT_OF_WORD = 3;
 
-    public String sortInnerContent(String text){
+    public String sortInnerContent(String text) {
         String result = "";
         String[] words = text.split(" ");
-        for (int i = 0; i < words.length; i++){
+        for (int i = 0; i < words.length; i++) {
             int wordLength = words[i].length();
-            if(wordLength > 3){
+            if (wordLength > MINIMUM_LENGHT_OF_WORD) {
                 StringBuilder innerWord = new StringBuilder(words[i].substring(1, wordLength - 1));
                 innerWord = sortDescending(innerWord);
-                words[i] = innerWord.append(words[i].charAt(wordLength-1)).insert(0, words[i].charAt(0)).toString();
+                words[i] = innerWord.append(words[i].charAt(wordLength - 1)).insert(0, words[i].charAt(0)).toString();
             }
         }
         result = String.join(" ", words);
@@ -20,13 +21,12 @@ public class SorterInnerContent {
     private StringBuilder sortDescending(StringBuilder text) {
         StringBuilder sorted = new StringBuilder();
         sorted.append(text.charAt(0));
-        for(int j = 1; j < text.length(); j++){
-            for (int k = 0; k < sorted.length(); k++){
-                if(text.charAt(j) > sorted.charAt(k)){
+        for (int j = 1; j < text.length(); j++) {
+            for (int k = 0; k < sorted.length(); k++) {
+                if (text.charAt(j) > sorted.charAt(k)) {
                     sorted.insert(k, text.charAt(j));
                     break;
-                }
-                else if(k == sorted.length()-1){
+                } else if (k == sorted.length() - 1) {
                     sorted.append(text.charAt(j));
                     break;
                 }
