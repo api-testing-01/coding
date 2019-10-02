a3636fdab5e9a2f7bc00c5357370f680992f925d
Luis Cachi <Luis Cachi@jalasoft.local>
2019-10-02 15:15:50 -0400

    Summary ======= Solving gradle check obs

--- src/main/java/org/fundacionjala/coding/luiscachi/SortDescending.java
+++ src/main/java/org/fundacionjala/coding/luiscachi/SortDescending.java
@@ -4,18 +4,21 @@ import java.util.Arrays;
 
 
 public class SortDescending {
-    public String sortDescending(String sortDescending) {
-        String wordsToSort[] = sortDescending.split(" ");
-        for (int i = 0; i < wordsToSort.length ; i++) {
-            if (wordsToSort[i].length() > 3) {
+    public String sortDescending(final String sortDescending) {
+        final String delimiter = " ";
+        final int minLenght = 3;
+        String[] wordsToSort = sortDescending.split(delimiter);
+        for (int i = 0; i < wordsToSort.length; i++) {
+            if (wordsToSort[i].length() > minLenght) {
                 String wordToSort = wordsToSort[i].substring(1, wordsToSort[i].length() - 1);
                 char[] arrayToSort = wordToSort.toCharArray();
                 Arrays.sort(arrayToSort);
                 String wordSort =  new String(arrayToSort);
                 String sortDes = new StringBuilder(wordSort).reverse().toString();
-                wordsToSort[i] = wordsToSort[i].charAt(0) + sortDes + wordsToSort[i].charAt(wordsToSort[i].length() - 1);
+                wordsToSort[i] = wordsToSort[i].charAt(0)
+                        + sortDes + wordsToSort[i].charAt(wordsToSort[i].length() - 1);
             }
         }
-        return String.join(" ", wordsToSort);
+        return String.join(delimiter, wordsToSort);
     }
 }
