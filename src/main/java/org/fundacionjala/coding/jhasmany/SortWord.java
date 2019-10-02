20061e30ce6a99877dabcbaa2a771fbb90859493
jhasmany_quiroz <jhasmany.quiroz@jalasoft.com>
2019-10-02 07:35:55 -0400

    Add the class to sort a word

---
+++ src/main/java/org/fundacionjala/coding/jhasmany/SortWord.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.coding.jhasmany;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SortWord {
+    public String sortTheInnerContent(String words) {
+        String[] wordList = words.split(" ");
+        StringBuilder response = new StringBuilder();
+        int secondIndex = 1, penultimateIndex;
+        StringBuilder aux;
+
+        for (String word : wordList) {
+            penultimateIndex = word.length() - 1;
+            aux = new StringBuilder(word);
+
+            // Deleting the first and the last character
+            String innerWord = word.substring(secondIndex, penultimateIndex);
+            String[] tempArray = innerWord.split("");
+            Arrays.sort(tempArray, Collections.reverseOrder());
+
+            String newWord = String.join("", tempArray);
+            aux = aux.replace(secondIndex, penultimateIndex, newWord);
+
+            response = response.append(aux).append(" ");
+        }
+
+        return response.toString();
+    }
+}
