12888567bf80197cbf48c2ff6670cee499aba707
edgarquispe <edgartron32@gmail.com>
2019-10-01 22:32:22 -0400

    sort inner word task01 update

--- src/main/java/org/fundacionjala/coding/edgar/SortTheInnerContent.java
+++ src/main/java/org/fundacionjala/coding/edgar/SortTheInnerContent.java
@@ -7,12 +7,12 @@ import java.util.List;
 final class SortTheInnerContent {
     private SortTheInnerContent() {
     }
-    public static final int LimitFlag = 3;
+    public static final int l = 3;
     public static String sortTheInnerContent(String words) {
         List<String> listOfWords = Arrays.asList(words.split(" "));
         words = "";
         for (String tempWord : listOfWords) {
-            if (tempWord.length() <= LimitFlag) {
+            if (tempWord.length() <= l) {
                 words += tempWord + " ";
             } else {
                 ArrayList<String> tempLetters = new ArrayList<>(Arrays.asList(tempWord.split("")));
