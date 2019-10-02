72a268a045780b840a818c109888130da8bdf739
Luis Cachi <Luis Cachi@jalasoft.local>
2019-10-02 15:17:21 -0400

    Spell error

--- src/main/java/org/fundacionjala/coding/luiscachi/SortDescending.java
+++ src/main/java/org/fundacionjala/coding/luiscachi/SortDescending.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class SortDescending {
     public String sortDescending(final String sortDescending) {
         final String delimiter = " ";
-        final int minLenght = 3;
+        final int minLength = 3;
         String[] wordsToSort = sortDescending.split(delimiter);
         for (int i = 0; i < wordsToSort.length; i++) {
-            if (wordsToSort[i].length() > minLenght) {
+            if (wordsToSort[i].length() > minLength) {
                 String wordToSort = wordsToSort[i].substring(1, wordsToSort[i].length() - 1);
                 char[] arrayToSort = wordToSort.toCharArray();
                 Arrays.sort(arrayToSort);
