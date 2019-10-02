544601a087fbf6777ebb3cf934c5b63087b0bbc9
Magali Fuentes <magali.fa@gmail.com>
2019-10-01 21:47:32 -0400

    updates

--- src/test/java/org/fundacionjala/coding/magali/SortContentTest.java
+++ src/test/java/org/fundacionjala/coding/magali/SortContentTest.java
@@ -6,7 +6,7 @@ public class SortContentTest {
 
     @Test
     public void test() {
-        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", SortContent.sortTheInnerContent("sort the inner content in descending order"));
+        assertEquals("srot the inner ctonnet", SortContent.sortTheInnerContent("sort the inner content"));
         assertEquals("wiat for me", SortContent.sortTheInnerContent("wait for me"));
         assertEquals("tihs ktaa is esay", SortContent.sortTheInnerContent("this kata is easy"));
     }
