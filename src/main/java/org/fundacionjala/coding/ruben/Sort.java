1c2b358b5a246f9b3ebd872975e340d22b2880a8
Ruben Mendoza <ruben.mendoza.jala@gmail.com>
2019-10-02 16:33:42 -0400

    Improve code with Gradle check advices

--- src/main/java/org/fundacionjala/coding/ruben/Sort.java
+++ src/main/java/org/fundacionjala/coding/ruben/Sort.java
@@ -1,13 +1,18 @@
 package org.fundacionjala.coding.ruben;
 
-public class Sort {
-    public static String sortTheInnerContent(String sentence, String regex) {
+final class Sort {
+
+    private Sort() {
+        //not called
+    }
+
+    private static String sortTheInnerContent(final String sentence, final String regex) {
         String[] words = sentence.split(regex);
         StringBuilder newWord = new StringBuilder();
         for (String word : words) {
             String firstLetter = word.substring(0, 1);
-            String lastLetter = word.substring(word.length() -1);
-            StringBuilder innerLetters = new StringBuilder(word.substring(1, word.length() -1));
+            String lastLetter = word.substring(word.length() - 1);
+            StringBuilder innerLetters = new StringBuilder(word.substring(1, word.length() - 1));
             for (int aux1 = 0; aux1 < innerLetters.length() - 1; aux1++) {
                 for (int aux2 = aux1 + 1; aux2 < innerLetters.length(); aux2++) {
                     if (innerLetters.charAt(aux1) < innerLetters.charAt(aux2)) {
@@ -17,12 +22,12 @@ public class Sort {
                     }
                 }
             }
-            newWord.append(firstLetter + innerLetters.toString() + lastLetter + regex);
+            newWord.append(firstLetter).append(innerLetters.toString()).append(lastLetter).append(regex);
         }
         return String.join(regex, newWord);
     }
 
-    public static String validateSentence(String sentence, String regex) {
+    public static String validateSentence(final String sentence, final String regex) {
         if (sentence.isEmpty()) {
             System.out.println("error, sentence is empty");
         } else {
--- src/test/java/org/fundacionjala/coding/ruben/SortInnerContentTest.java
+++ src/test/java/org/fundacionjala/coding/ruben/SortInnerContentTest.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SortInnerContentTest {
+
     @Test
     public void sentenceWithSpaceRegex() {
-        assertEquals("srot the inner ctonnet in dsnnieedcg oredr ", Sort.validateSentence("sort the inner content in descending order", " "));
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr ",
+                Sort.validateSentence("sort the inner content in descending order", " "));
     }
 
     @Test
-    public void sentenceWithAnotherRegex(){
+    public void sentenceWithAnotherRegex() {
         assertEquals("wiat-for-me-", Sort.validateSentence("wait-for-me", "-"));
+
     }
 }
