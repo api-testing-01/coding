ec97cb63a0a4ec5492cc20e7f3da10db4fd9e9bb
Luis Cachi <Luis Cachi@jalasoft.local>
2019-10-02 15:00:08 -0400

    Summary ======= Solve Kata

---
+++ src/main/java/org/fundacionjala/coding/luiscachi/SortDescending.java
@@ -0,0 +1,21 @@
+package org.fundacionjala.coding.luiscachi;
+
+import java.util.Arrays;
+
+
+public class SortDescending {
+    public String sortDescending(String sortDescending) {
+        String wordsToSort[] = sortDescending.split(" ");
+        for (int i = 0; i < wordsToSort.length ; i++) {
+            if (wordsToSort[i].length() > 3) {
+                String wordToSort = wordsToSort[i].substring(1, wordsToSort[i].length() - 1);
+                char[] arrayToSort = wordToSort.toCharArray();
+                Arrays.sort(arrayToSort);
+                String wordSort =  new String(arrayToSort);
+                String sortDes = new StringBuilder(wordSort).reverse().toString();
+                wordsToSort[i] = wordsToSort[i].charAt(0) + sortDes + wordsToSort[i].charAt(wordsToSort[i].length() - 1);
+            }
+        }
+        return String.join(" ", wordsToSort);
+    }
+}
