c7f23fb69215ec4cd984a02b9fcea73104c558a8
Mary Ricalde <mricalde@pros.com>
2019-10-01 20:41:54 -0400

    Sort inner content

---
+++ src/main/java/org/fundacionjala/coding/mary/InnerContent.java
@@ -0,0 +1,49 @@
+package org.fundacionjala.coding.mary;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by Mary Ricalde on 10/1/2019.
+ */
+public final class InnerContent {
+    private static final int WORD_LENGTH = 3;
+
+    private InnerContent() {
+    }
+
+    public static String sortTheInnerContent(final String words) {
+        String result;
+
+        if (words == null) {
+            result = "Entered null string";
+            return result;
+        }
+
+        if (words.length() > 0) {
+            String[] wordsList = words.split(" ");
+            for (int i = 0; i < wordsList.length; i++) {
+                if (wordsList[i].length() > WORD_LENGTH) {
+                    wordsList[i] = descentOrderText(wordsList[i]);
+                }
+            }
+            result = String.join(" ", wordsList);
+        } else {
+            result = "Entered empty string";
+        }
+
+        return result;
+    }
+
+    public static String descentOrderText(final String text) {
+        String result;
+        int startIndex = 0;
+        int endIndex = text.length() - 1;
+        String innerContent = text.substring(startIndex + 1, endIndex);
+        String[] innerContentList = innerContent.split("");
+        Arrays.sort(innerContentList, Collections.reverseOrder());
+        result = text.charAt(startIndex) + String.join("", innerContentList) + text.charAt(endIndex);
+
+        return result;
+    }
+}
