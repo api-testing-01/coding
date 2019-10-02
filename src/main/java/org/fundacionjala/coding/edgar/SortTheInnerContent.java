eaedcf592e6f33afa015ccca4f749d840c2c350c
edgarquispe <edgartron32@gmail.com>
2019-10-01 22:48:26 -0400

    sort inner word task01 update

--- src/main/java/org/fundacionjala/coding/edgar/SortTheInnerContent.java
+++ src/main/java/org/fundacionjala/coding/edgar/SortTheInnerContent.java
@@ -8,21 +8,23 @@ final class SortTheInnerContent {
     private SortTheInnerContent() {
     }
 
-    private static final int Limit = 3;
+    private static final int FLAG = 3;
 
-    static String sortTheInnerContent(String words) {
+    static String sortTheInnerContent(final String words) {
         String[] arrayWords = words.split(" ");
         StringBuilder newWord = new StringBuilder();
 
         for (String str : arrayWords) {
             String temp = "";
-            if (str.length() > Limit) {
+            if (str.length() > FLAG) {
                 char[] letter = str.toCharArray();
                 String reorderContent = str.substring(1, str.length() - 1).chars().mapToObj(e -> (char) e)
                         .sorted(Collections.reverseOrder()).collect(Collectors.toList()).stream()
                         .map(Objects::toString).collect(Collectors.joining(""));
                 temp = letter[0] + reorderContent + letter[letter.length - 1] + " ";
-            } else temp += str + " ";
+            } else{
+                temp += str + " ";
+            }
             newWord.append(temp);
         }
         return newWord.toString().trim();
