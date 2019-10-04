beaf0a5b0ad417d2eed86ede8f10ce4b2d81aed0
bhuanca <benjamin.huanca.jala@gmail.com>
2019-10-04 16:58:25 -0400

    Missing test class.

---
+++ src/test/java/org/fundacionjala/coding/benjamin/SolutionTest.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.coding.benjamin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Benjamin Huanca on 9/30/2019.
+ * @version 1.0
+ */
+public class SolutionTest {
+    @Test
+    public void exampleTests() {
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr",
+                Kata.sortTheInnerContent("sort the inner content in descending order"));
+        assertEquals("wiat for me", Kata.sortTheInnerContent("wait for me"));
+        assertEquals("tihs ktaa is esay", Kata.sortTheInnerContent("this kata is easy"));
+        assertEquals("t", Kata.sortTheInnerContent("t"));
+    }
+}
