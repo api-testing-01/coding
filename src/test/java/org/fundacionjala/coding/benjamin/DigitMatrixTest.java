f597f53f5fecfc292963539b7ee7f144400ba2d6
bhuanca <benjamin.huanca.jala@gmail.com>
2019-10-03 21:45:18 -0400

    First part of the Kata BankOCR

---
+++ src/test/java/org/fundacionjala/coding/benjamin/DigitMatrixTest.java
@@ -0,0 +1,46 @@
+package org.fundacionjala.coding.benjamin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Benjamin Huanca on 10/2/2019.
+ * @version 1.0
+ */
+public class DigitMatrixTest {
+    private static final int MATRIX_DIMENSION = 3;
+
+    @Test
+    public void getCharactersNumberTest() {
+        char[][] characterMatrix = {{' ', '_', ' '}, {' ', '_', '|'}, {' ', '_', '|'}, {' ', ' ', ' '}};
+        DigitMatrix digitMatrix = new DigitMatrix();
+        char expected = '3';
+        assertEquals(expected, digitMatrix.getCharacter(characterMatrix));
+    }
+
+    @Test
+    public void getCharactersLTest() {
+        char[][] characterMatrix = {{' ', '_', ' '}, {' ', ' ', '|'}, {' ', '_', '|'}, {' ', ' ', ' '}};
+        DigitMatrix digitMatrix = new DigitMatrix();
+        char expected = '?';
+        assertEquals(expected, digitMatrix.getCharacter(characterMatrix));
+    }
+
+    @Test
+    public void decode0To8Test() {
+        char[][] lcdMatrix = {
+                {' ', '_', ' ', ' ', ' ', ' ', ' ', '_', ' ', ' ', '_', ' ', ' ', ' ', ' ', ' ', '_', ' ',
+                        ' ', '_', ' ', ' ', '_', ' ', ' ', '_', ' '},
+                {'|', ' ', '|', ' ', ' ', '|', ' ', '_', '|', ' ', '_', '|', '|', '_', '|', '|', '_', ' ',
+                        '|', '_', ' ', ' ', ' ', '|', '|', '_', '|'},
+                {'|', '_', '|', ' ', ' ', '|', '|', '_', ' ', ' ', '_', '|', ' ', ' ', '|', ' ', '_', '|',
+                        '|', '_', '|', ' ', ' ', '|', '|', '_', '|'},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+                        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
+        String expected = "012345678";
+        DigitMatrix digitMatrix = new DigitMatrix();
+
+        assertEquals(expected, digitMatrix.decoder(lcdMatrix));
+    }
+}
