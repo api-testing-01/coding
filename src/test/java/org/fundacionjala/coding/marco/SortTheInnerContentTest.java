35594ac65901a8733fd3cb863f6415a51ebad1d6
marcomendieta <marco.mendieta@jalasoft.com>
2019-10-14 07:52:59 -0400

    marco/short_inner_content add kata (#11)
    * marco/short_inner_content add kata
    
    * fix code style error

---
+++ src/test/java/org/fundacionjala/coding/marco/SortTheInnerContentTest.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.coding.marco;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SortTheInnerContentTest {
+
+    @Test
+    public void sortTheInnerContentTest() {
+        assertEquals(
+                "srot the inner ctonnet in dsnnieedcg oredr",
+                SortTheInnerContent.sortTheInnerContent(
+                        "sort the inner content in descending order"));
+        assertEquals(
+                "wiat for me",
+                SortTheInnerContent.sortTheInnerContent("wait for me"));
+        assertEquals(
+                "tihs ktaa is esay",
+                SortTheInnerContent.sortTheInnerContent("this kata is easy"));
+    }
+}
