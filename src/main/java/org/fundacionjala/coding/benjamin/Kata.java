dee5e4f55e7c68659553b4457b73443aa5566282
bhuanca <benjamin.huanca.jala@gmail.com>
2019-10-16 19:18:26 -0400

    Sort inner content solution.

---
+++ src/main/java/org/fundacionjala/coding/benjamin/Kata.java
@@ -0,0 +1,36 @@
+package org.fundacionjala.coding.benjamin;
+
+import java.util.Arrays;
+/**
+ * @author Benjamin Huanca on 10/16/2019.
+ * @version 1.0
+ */
+final class Kata {
+    /**
+     * Private constructor.
+     */
+    private Kata() {
+    }
+
+    public static String sortTheInnerContent(final String words) {
+        final int minimumCharLength = 2;
+        final String delimiter = " ";
+        final String[] phrase = words.split(delimiter);
+        String[] sortedString = new String[phrase.length];
+
+        for (int i = 0; i < phrase.length; i++) {
+            if (phrase[i].length() > minimumCharLength) {
+                char[] tempArray = phrase[i].substring(1, phrase[i].length() - 1).toCharArray();
+                Arrays.sort(tempArray);
+                StringBuilder reverse = new StringBuilder();
+                reverse.append(tempArray);
+                sortedString[i] = phrase[i].charAt(0) + reverse.reverse().toString()
+                        + phrase[i].charAt(phrase[i].length() - 1);
+            } else {
+                sortedString[i] = phrase[i];
+            }
+
+        }
+        return String.join(delimiter, sortedString);
+    }
+}
