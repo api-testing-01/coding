7a2bfbc3761339f0216376bbc8e870af39381e38
LF <lufer.lfcc@gmail.com>
2019-10-30 15:43:43 -0400

    summary ======= - Solve Kata EAN number - add unit test

--- src/main/java/org/fundacionjala/coding/luiscachi/EANValidation.java
+++ src/main/java/org/fundacionjala/coding/luiscachi/EANValidation.java
@@ -1,4 +1,21 @@
 package org.fundacionjala.coding.luiscachi;
 
 public class EANValidation {
+    public boolean EANValidation(String EAN){
+        String[] numbers = EAN.split("");
+        int EANNumber = 0;
+        int checkSum = 0;
+        for (int i = 0; i <numbers.length-1 ; i++) {
+            if(i%2 == 0){
+                checkSum += (Integer.parseInt(numbers[i])*1);
+            }else{
+                checkSum += (Integer.parseInt(numbers[i])*3);
+            }
+        }
+        if (checkSum%10 != 0 )
+            EANNumber = 10 - (checkSum%10);
+        if (numbers.length == 13 && EANNumber == Integer.parseInt(numbers[numbers.length-1]))
+            return true;
+        else return false;
+    }
 }
--- src/test/java/org/fundacionjala/coding/luiscachi/EANValitadionTest.java
+++ src/test/java/org/fundacionjala/coding/luiscachi/EANValitadionTest.java
@@ -1,4 +1,46 @@
 package org.fundacionjala.coding.luiscachi;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EANValitadionTest {
+    @Test
+    public void testIsEanNumber(){
+        String EANNumber = "4003301018398";
+        EANValidation eanValidation = new EANValidation();
+        boolean result = eanValidation.EANValidation(EANNumber);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsNotEanNumber(){
+        String EANNumber = "4003301018392";
+        EANValidation eanValidation = new EANValidation();
+        boolean result = eanValidation.EANValidation(EANNumber);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testIsEANNumber0000000000000(){
+        String EANNumber = "0000000000000";
+        EANValidation eanValidation = new EANValidation();
+        boolean result = eanValidation.EANValidation(EANNumber);
+        assertEquals(true, result);
+    }
+    @Test
+    public void testIsEANNumber123(){
+        String EANNumber = "123";
+        EANValidation eanValidation = new EANValidation();
+        boolean result = eanValidation.EANValidation(EANNumber);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testIsEANNumberALargeNumber(){
+        String EANNumber = "9876543216821654654";
+        EANValidation eanValidation = new EANValidation();
+        boolean result = eanValidation.EANValidation(EANNumber);
+        assertEquals(false, result);
+    }
 }
