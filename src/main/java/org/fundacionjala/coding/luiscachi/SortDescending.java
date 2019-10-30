0c019868f6137fc6720d334957a7634a3a65b22a
LF <lufer.lfcc@gmail.com>
2019-10-30 16:21:09 -0400

    Summary ======= Solve Gradle checklist

--- src/main/java/org/fundacionjala/coding/luiscachi/SortDescending.java
+++ src/main/java/org/fundacionjala/coding/luiscachi/SortDescending.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 
 
 public class SortDescending {
-    public String sortDescending(String sortDescending) {
-        String wordsToSort[] = sortDescending.split(" ");
-        for (int i = 0; i < wordsToSort.length ; i++) {
-            if (wordsToSort[i].length() > 3) {
+    public String sortDescending(final String sortDescending) {
+        final int maxWordLength = 3;
+        String[] wordsToSort = sortDescending.split(" ");
+        for (int i = 0; i < wordsToSort.length; i++) {
+            if (wordsToSort[i].length() > maxWordLength) {
                 String wordToSort = wordsToSort[i].substring(1, wordsToSort[i].length() - 1);
                 char[] arrayToSort = wordToSort.toCharArray();
                 Arrays.sort(arrayToSort);
                 String wordSort =  new String(arrayToSort);
                 String sortDes = new StringBuilder(wordSort).reverse().toString();
-                wordsToSort[i] = wordsToSort[i].charAt(0) + sortDes + wordsToSort[i].charAt(wordsToSort[i].length() - 1);
+                wordsToSort[i] = wordsToSort[i].charAt(0) + sortDes
+                        + wordsToSort[i].charAt(wordsToSort[i].length() - 1);
             }
         }
         return String.join(" ", wordsToSort);
--- src/test/java/org/fundacionjala/coding/luiscachi/EANValitadionTest.java
+++ src/test/java/org/fundacionjala/coding/luiscachi/EANValitadionTest.java
@@ -6,41 +6,41 @@ import static org.junit.Assert.assertEquals;
 
 public class EANValitadionTest {
     @Test
-    public void testIsEanNumber(){
-        String EANNumber = "4003301018398";
-        EANValidation eanValidation = new EANValidation();
-        boolean result = eanValidation.EANValidation(EANNumber);
+    public void testIsEanNumber() {
+        String eanNumber = "4003301018398";
+        EANNumberValidation eanValidation = new EANNumberValidation();
+        boolean result = eanValidation.eanValidation(eanNumber);
         assertEquals(true, result);
     }
 
     @Test
-    public void testIsNotEanNumber(){
-        String EANNumber = "4003301018392";
-        EANValidation eanValidation = new EANValidation();
-        boolean result = eanValidation.EANValidation(EANNumber);
+    public void testIsNotEanNumber() {
+        String eanNumber = "4003301018392";
+        EANNumberValidation eanValidation = new EANNumberValidation();
+        boolean result = eanValidation.eanValidation(eanNumber);
         assertEquals(false, result);
     }
 
     @Test
-    public void testIsEANNumber0000000000000(){
-        String EANNumber = "0000000000000";
-        EANValidation eanValidation = new EANValidation();
-        boolean result = eanValidation.EANValidation(EANNumber);
+    public void testIsEANNumber0000000000000() {
+        String eanNumber = "0000000000000";
+        EANNumberValidation eanValidation = new EANNumberValidation();
+        boolean result = eanValidation.eanValidation(eanNumber);
         assertEquals(true, result);
     }
     @Test
-    public void testIsEANNumber123(){
-        String EANNumber = "123";
-        EANValidation eanValidation = new EANValidation();
-        boolean result = eanValidation.EANValidation(EANNumber);
+    public void testIsEANNumber123() {
+        String eanNumber = "123";
+        EANNumberValidation eanValidation = new EANNumberValidation();
+        boolean result = eanValidation.eanValidation(eanNumber);
         assertEquals(false, result);
     }
 
     @Test
-    public void testIsEANNumberALargeNumber(){
-        String EANNumber = "9876543216821654654";
-        EANValidation eanValidation = new EANValidation();
-        boolean result = eanValidation.EANValidation(EANNumber);
+    public void testIsEANNumberALargeNumber() {
+        String eanNumber = "9876543216821654654";
+        EANNumberValidation eanValidation = new EANNumberValidation();
+        boolean result = eanValidation.eanValidation(eanNumber);
         assertEquals(false, result);
     }
 }
