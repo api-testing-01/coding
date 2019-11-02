260b12a56a89f99e812332d69869da372ac494a5
jccolina <jose.colina@jalasoft.com>
2019-11-01 20:59:46 -0400

    Added test fr invalid account

--- src/test/java/org/fundacionjala/coding/joseColina/TestChecksum.java
+++ src/test/java/org/fundacionjala/coding/joseColina/TestChecksum.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 public class TestChecksum {
     @Test
     public void testChecksumValid(){
-        String accountNumber = "999215810";
+        String accountNumber = "999215817";
         Checksum checksum = new Checksum();
         Assert.assertTrue(checksum.isAccountValid(accountNumber));
+    }
+    @Test
+    public void testChecksumNotValid(){
+        String accountNumber = "999215810";
+        Checksum checksum = new Checksum();
+        Assert.assertFalse(checksum.isAccountValid(accountNumber));
     }
 }
