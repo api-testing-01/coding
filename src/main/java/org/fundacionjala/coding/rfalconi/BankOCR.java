6cc49f56bb08c21f51d95de5bd9dbe57516fb855
unknown <Rosario@RFALCONIH-DV01.jalasoft.local>
2019-11-12 17:17:29 -0400

    Updating BankOCR files

--- src/main/java/org/fundacionjala/coding/rfalconi/BankOCR.java
+++ src/main/java/org/fundacionjala/coding/rfalconi/BankOCR.java
@@ -3,8 +3,16 @@ package org.fundacionjala.coding.rfalconi;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BankOCR {
+
+    private static final Logger LOGGER = Logger.getLogger("org.fundacionjala.coding.rfalconi.BankOCR");
+
     private static int MODULE = 11;
 
     public void userStory1() {
@@ -25,13 +33,18 @@ public class BankOCR {
             position--;
         }
         sum = sum % MODULE;
+        LOGGER.log( Level.INFO, "Sum result {0}", sum);
         return sum;
     }
 
     public boolean validateUserStory2(final String accountId){
-        int sum = userStory2(accountId);
-        if (sum == 0) {
-            return true;
+        if (isNumeric(accountId)) {
+            int sum = userStory2(accountId);
+            if (sum == 0) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
@@ -40,16 +53,7 @@ public class BankOCR {
     public String userStory3(final String accountId) {
         String filename = "src\\test\\java\\org\\fundacionjala\\coding\\rfalconi\\bankOCR.txt";
         String accountLine ="";
-        String accountId2 = "";
-
-        char[] digits = String.valueOf(accountId).toCharArray();
-        for (int i = 0; i < digits.length; i++) {
-            System.out.println(Integer.parseInt(String.valueOf(digits[i])));
-
-        }
-        accountId2 = digits.toString();
         try {
-
             FileWriter fileWriter = new FileWriter(filename, true);
             if (accountId.contains("?")) {
                 accountLine = accountId.concat(" ILL");
@@ -63,14 +67,29 @@ public class BankOCR {
                     fileWriter.write(accountLine.concat("\n"));
                 }
             }
-
             fileWriter.flush();
             fileWriter.close();
-
         } catch (IOException err) {
             System.out.println("Error E/S: " + err);
         }
+        LOGGER.log( Level.INFO, "Line added to file:: {0}", accountLine);
         return accountLine;
+    }
+
+    public boolean isNumeric(final String accountId){
+        final List <String> digits = Arrays.asList(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"});
+        final char[] accountDigits = accountId.toCharArray();
+
+        boolean isNumeric = false;
+        for (int i = 0; i < accountDigits.length; i++) {
+            if(digits.contains(String.valueOf(accountDigits[i]))){
+                isNumeric = true;
+            } else{
+                isNumeric = false;
+                break;
+            }
+        }
+        return isNumeric;
     }
 
     public void fileReader(final String filename) {
--- src/test/java/org/fundacionjala/coding/rfalconi/BankOCRTest.java
+++ src/test/java/org/fundacionjala/coding/rfalconi/BankOCRTest.java
@@ -8,19 +8,16 @@ public class BankOCRTest {
     @Test
     public void userStory2Test() {
         BankOCR ocr = new BankOCR();
-        Assert.assertEquals(0, ocr.userStory2("345882865"));
         Assert.assertTrue("Account is valid", ocr.validateUserStory2("345882865"));
-        Assert.assertEquals(0, ocr.userStory2("457508000"));
         Assert.assertTrue("Account is valid", ocr.validateUserStory2("457508000"));
-        Assert.assertNotEquals(0, ocr.userStory2("664371495"));
-        Assert.assertFalse("Account is not valid", ocr.validateUserStory2("664371495"));
+        Assert.assertFalse("Account is not valid", ocr.validateUserStory2("6643714>5"));
     }
 
     @Test
     public void userStory3Test() {
         BankOCR ocr = new BankOCR();
-        //Assert.assertEquals("345882865", ocr.userStory3("345882865"));
-        //Assert.assertEquals("664371495 ERR", ocr.userStory3("664371495"));
+        Assert.assertEquals("345882865", ocr.userStory3("345882865"));
+        Assert.assertEquals("664371495 ERR", ocr.userStory3("664371495"));
         Assert.assertEquals("66437149? ILL", ocr.userStory3("66437149A"));
 
     }
