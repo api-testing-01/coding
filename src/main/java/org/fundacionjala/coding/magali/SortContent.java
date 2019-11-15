cf4f8b4482656f001a8b4127ee8f01f91010ee53
Magali Fuentes <magali.fa@gmail.com>
2019-11-15 18:30:39 -0400

    updated the class sortTheInnerContent

--- src/main/java/org/fundacionjala/coding/magali/SortContent.java
+++ src/main/java/org/fundacionjala/coding/magali/SortContent.java
@@ -12,29 +12,22 @@ public final class SortContent {
 
     public static String sortTheInnerContent(final String words) {
         String[] wordArray = words.split(" ");
-        String[] wordSorted = new String[wordArray.length];
-
         for (int i = 0; i < wordArray.length; i++) {
             if (wordArray[i].length() > WORD_LENGTH) {
                 String sortedWord = wordArray[i].substring(1, wordArray[i].length() - 1);
                 char[] sortedChars = sortedWord.toCharArray();
                 Arrays.sort(sortedChars);
-
                 for (int j = 0; j < sortedChars.length / 2; j++) {
                     char tmp = sortedChars[j];
                     int index = sortedChars.length - 1 - j;
                     sortedChars[j] = sortedChars[index];
                     sortedChars[index] = tmp;
                 }
-
-                wordSorted[i] = wordArray[i].charAt(0) + new String(sortedChars)
+                wordArray[i] = wordArray[i].charAt(0) + new String(sortedChars)
                         + wordArray[i].charAt(wordArray[i].length() - 1);
-            } else {
-                wordSorted[i] = wordArray[i];
             }
-
         }
-        return String.join(" ", wordSorted);
+        return String.join(" ", wordArray);
     }
 }
 
