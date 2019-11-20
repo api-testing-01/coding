64e1edff7a50de342a596f8f1a4a51e7b5eaaf87
Ruben Mendoza <ruben.mendoza.jala@gmail.com>
2019-11-20 12:02:14 -0400

    Fix merge issues

--- src/main/java/org/fundacionjala/coding/ruben/EANValidator.java
+++ src/main/java/org/fundacionjala/coding/ruben/EANValidator.java
@@ -2,7 +2,6 @@ package org.fundacionjala.coding.ruben;
 
 public final class EANValidator {
 
-<<<<<<< HEAD
     private static final int MULTIPLIER_NUMBER = 3;
     private static final int MOD_NUMBER = 10;
     private static final int LAST_DIGIT = 12;
@@ -25,33 +24,5 @@ public final class EANValidator {
 
         int checkSum = (sum % MOD_NUMBER);
         return checkSum == Character.getNumericValue(eanCode.charAt(LAST_DIGIT));
-=======
-    public static final int ODD_NUMBER_MULTIPLIER = 3;
-    public static final int CHECKSUM_CALCULATOR = 10;
-
-    private EANValidator() {
-        // private constructor
-    }
-
-    public static boolean validate(final String eanCode) {
-        String[] result = eanCode.split("");
-        int[] eanNumbers = new int[eanCode.length()];
-        int sum = 0;
-        for (int count = 0; count < result.length; count++) {
-            eanNumbers[count] = Integer.parseInt(result[count]);
-        }
-        for (int count = 1; count < eanNumbers.length - 1; count = count + 2) {
-            eanNumbers[count] = eanNumbers[count] * ODD_NUMBER_MULTIPLIER;
-        }
-        for (int count = 0; count < eanNumbers.length - 1; count++) {
-            sum = sum + eanNumbers[count];
-        }
-        int checkSum = CHECKSUM_CALCULATOR - (sum % CHECKSUM_CALCULATOR);
-        if (checkSum == eanNumbers[eanNumbers.length - 1]) {
-            return true;
-        } else {
-            return checkSum == CHECKSUM_CALCULATOR && eanNumbers[result.length - 1] == 0;
-        }
->>>>>>> b521de959ac1d1e9045496e60de85cd339142d8f
     }
 }
